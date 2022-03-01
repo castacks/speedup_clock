c8b3198a0333ff1d102a438a3394d75fdfd135e9
YuanJunbin <jyuanb@gmail.com>
2022-02-28 23:31:40 -0500

    a clock

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup 
+from catkin_pkg.python_setup import generate_distutils_setup
+
+setup_args = generate_distutils_setup(
+    version="0.0.0",
+    packages=["speedup_clock"],
+    package_dir={'': 'src'}
+)
+
+setup(**setup_args)
